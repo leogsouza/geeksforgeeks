17d65e8c99b8fb724fcf4c0a65edd4f2c03ab664
Leonardo Gonçalves <ldsz@gft.com>
2019-01-08 17:47:27 -0200

    Add unit test

---
+++ algorithms/linear-search/linear-search.go
@@ -0,0 +1,11 @@
+package linear-search
+
+func linearSearch(arr []int, x int) int {
+	for key, item := range arr {
+		if item == x {
+			return key
+		}
+	}
+
+	return -1
+}
---
+++ algorithms/linear-search/linear-search_test.go
@@ -0,0 +1,22 @@
+package linear-search
+
+import (
+	"testing"
+)
+
+func TestLinearSearch(t *testing.T) {
+	data := []struct {
+		arr              []int
+		search, expected int
+	}{
+		{[]int{1, 2, 3, 4, 5, 6}, 5, 4},
+		{[]int{11, 22, 43, 54, 15, 46}, 45, -1},
+		{[]int{1, 12, 23, 51, 12, 45}, 45, 5},
+	}
+
+	for _, d := range data {
+		if got := linearSearch(d.arr, d.search); got != d.expected {
+			t.Errorf("LinearSearch expected %d but got %d", d.expected, got)
+		}
+	}
+}
