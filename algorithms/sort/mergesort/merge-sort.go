16c6ea136a625da7ccfc15f67472d8b578b4922b
Leonardo Souza <leogsouza@gmail.com>
2019-03-06 22:16:27 -0300

    Finish Merge Sort Algorithm

--- algorithms/sort/mergesort/merge-sort.go
+++ algorithms/sort/mergesort/merge-sort.go
@@ -5,18 +5,44 @@ func split(arr []int) ([]int, []int) {
 	return arr[0:mid], arr[mid:]
 }
 
-func sort(arr []int) ([]int, []int) {
+func sort(arr []int) []int {
 	if len(arr) <= 1 {
-		return nil, arr
+		return arr
 	}
 
 	l, r := split(arr)
-	return l, r
+	l = sort(l)
+	r = sort(r)
+	return merge(l, r)
 }
 
-func MergeSort(l, r []int) []int {
+func merge(l []int, r []int) []int {
+	arr := make([]int, len(l)+len(r))
 
-	arr := l
+	j, k := 0, 0
 
+	for i := 0; i < len(arr); i++ {
+		if j >= len(l) {
+			arr[i] = r[k]
+			k++
+			continue
+		} else if k >= len(r) {
+			arr[i] = l[j]
+			j++
+			continue
+		}
+
+		if l[j] > r[k] {
+			arr[i] = r[k]
+			k++
+		} else {
+			arr[i] = l[j]
+			j++
+		}
+	}
 	return arr
+}
+
+func MergeSort(arr []int) []int {
+	return sort(arr)
 }
