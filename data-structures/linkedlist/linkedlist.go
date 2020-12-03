8b4ecc7fb1591e6d7373569978a815b48a496a7a
Leonardo Gonçalves <ldsz@gft.com>
2020-12-03 17:54:29 -0300

    Add Linked List insert methods

--- data-structures/linkedlist/linkedlist.go
+++ data-structures/linkedlist/linkedlist.go
@@ -8,6 +8,14 @@ type Node struct {
 	Next *Node
 }
 
+// NewNode creates a pointer to a new Node
+func NewNode(data int) *Node {
+	newNode := &Node{}
+	newNode.Data = data
+
+	return newNode
+}
+
 // LinkedList is a linked list data structure
 type LinkedList struct {
 	Head *Node
@@ -20,4 +28,44 @@ func (ll *LinkedList) PrintList() {
 		fmt.Printf("%d ", n.Data)
 		n = n.Next
 	}
+}
+
+// AddToHead adds a new node into linked list head
+func (ll *LinkedList) AddToHead(data int) {
+
+	newNode := NewNode(data)
+
+	newNode.Next = ll.Head
+	ll.Head = newNode
+
+}
+
+// AddAfter adds a new node after the given prevNode
+func (ll *LinkedList) AddAfter(prevNode *Node, data int) {
+	if prevNode == nil {
+		fmt.Println("The given previous node cannot be null")
+	}
+
+	newNode := NewNode(data)
+
+	newNode.Next = prevNode.Next
+
+	prevNode.Next = newNode
+}
+
+// AddToEnd adds a new node into do end of the linked list
+func (ll *LinkedList) AddToEnd(data int) {
+	newNode := NewNode(data)
+
+	if ll.Head == nil {
+		ll.Head = newNode
+		return
+	}
+
+	last := ll.Head
+	for last.Next != nil {
+		last = last.Next
+	}
+
+	last.Next = newNode
 }
